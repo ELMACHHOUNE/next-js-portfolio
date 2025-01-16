477ab646908ab11b50659cdd0b0736b107bcd295
ELMACHHOUNE <mohamed20rida@gmail.com>
2025-01-16 17:03:04 +0100

    fix SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,10 @@ const RootLayout = ({ children }: Readonly<PropsWithChildren>) => {
   return (
     <html lang="en">
       <head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/icon.png" />
+        <link rel="canonical" href="https://www.elmachhoune.me/" />
       </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" forcedTheme="dark">
