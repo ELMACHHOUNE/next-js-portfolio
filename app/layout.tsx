4a394538c1255b3b74d4092cc54c6d700b829775
ELMACHHOUNE <mohamed20rida@gmail.com>
2025-01-16 18:19:10 +0100

    inject structured data (JSON-LD)

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,26 @@ const RootLayout = ({ children }: Readonly<PropsWithChildren>) => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/icon.png" />
         <link rel="canonical" href="https://www.elmachhoune.me/" />
+        <meta name="robots" content="index, follow" />
+        {/* Structured Data */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              name: "MOHAMED EL MACHHOUNE",
+              url: "https://www.elmachhoune.me/",
+              author: {
+                "@type": "Person",
+                name: "MOHAMED EL MACHHOUNE",
+                url: "https://github.com/ELMACHHOUNE/",
+              },
+              description:
+                "Mohamed EL MACHHOUNE's portfolio showcasing full-stack development, UX/UI design, and consulting expertise.",
+            }),
+          }}
+        ></script>
       </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" forcedTheme="dark">
